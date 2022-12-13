5ba6898622d99baa298192dfb289d9bd47f67ea3
Peirlberger Juergen <juergen.peirlberger@gmx.at>
2022-12-13 15:30:25 +0100

    create gradle app

--- Blackjack/src/test/java/blackjack/BlackjackTest.java
+++ Blackjack/src/test/java/blackjack/BlackjackTest.java
@@ -1,3 +1,5 @@
+package blackjack;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
--- Blackjack/src/test/java/blackjack/PlayerTest.java
+++ Blackjack/src/test/java/blackjack/PlayerTest.java
@@ -1,3 +1,5 @@
+package blackjack;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
